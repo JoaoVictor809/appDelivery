bfa004bf9811e01f44d3c00befaa0194591336d9
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-04-08 20:29:03 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { View, SafeAreaView, Text } from "react-native";
+
+
+export default function home(){
+  return(
+    <SafeAreaView>
+      <View>
+        localização e user
+        <View>
+          <Text></Text>
+          <Text></Text>
+        </View>
+
+        icon de compras
+        <View>
+          
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
