3f17d1e7678d8a0fb107c480c3e6ce808ab94b51
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-04-08 22:22:10 -0300

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,68 @@
-import { View, SafeAreaView, Text } from "react-native";
+import { View, SafeAreaView, Text, TextInput, ScrollView, Image } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from "expo-font";
+import { useState, useEffect } from 'react';
+import Estilos from '../assets/styles/home'
 
 
 export default function home(){
+  const [fontsLoaded] = useFonts({
+    'Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf')
+  });
+  
+  useEffect(() => {
+    if (fontsLoaded) {
+        SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) {
+    return null;
+  }
   return(
-    <SafeAreaView>
-      <View>
-        localização e user
+    <ScrollView showsVerticalScrollIndicator={false}>
+    <SafeAreaView style={{flex:1, width:'100%', backgroundColor:'#fff', gap:15, }}>
+      
+      <View style={Estilos.header}>
+       
         <View>
-          <Text></Text>
-          <Text></Text>
+          <Text style={{fontFamily:'Regular'}}>Localização</Text>
+          <Text style={{fontFamily:'Bold'}}>Rua Domingues, Barueri</Text>
         </View>
 
-        icon de compras
-        <View>
-          
+        
+        <View style={Estilos.icon}>
+          <Image 
+          source={require('../assets/images/Bag.svg')}/>
         </View>
       </View>
+
+      <View>
+        <TextInput
+        style={Estilos.input}
+        placeholder="Search"
+         />
+         <View style={Estilos.imageSearch}>
+            <Image source={require('../assets/images/Search.svg')} />
+         </View>
+      </View>
+      <View>
+        imagemPromo
+      </View>
+
+      <View>
+        <Text>
+          Categotias
+        </Text>
+      </View>
+      <View>
+        <Text>
+          Comidas
+        </Text>
+      </View>
     </SafeAreaView>
+    </ScrollView>
   )
 }
--- assets/styles/home.tsx
+++ assets/styles/home.tsx
@@ -5,6 +5,29 @@ export default StyleSheet.create({
 
     },
     header:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        paddingTop:15
+    },
+    icon:{
+        backgroundColor:'#E6E6E8',
+        padding:10,
+        borderRadius:30
+    },
+    boxInput:{
+
+    },
+    input:{
+        backgroundColor:'#E6E6E8',
+        height:50,
+        color:'#64646A',
+        fontFamily:'Regular',
+        borderRadius:30,
+        width:'80%',
+        paddingLeft:60
+    },
+    imageSearch:{
         
     }
 })
